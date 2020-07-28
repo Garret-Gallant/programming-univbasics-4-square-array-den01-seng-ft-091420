446782515c5628400be063a6fb4defa637a3393e
Garret Gallant <gallanttechnological@gmail.com>
2020-07-28 00:27:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  count = 0
+  new_array = [ ]
+  
+  while count < array.length {
+    new_array.push(array[count ** 2])
+    count += 1
+  }
+  new_array
+  
 end
